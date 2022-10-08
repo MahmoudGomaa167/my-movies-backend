685538378288e7c4b3a9a260aaf0bd4ccfe67758
MahmoudGomaa167 <mimogogo167@gmail.com>
2022-10-08 15:24:05 +0200

    third commit

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const runDB = () => {
-    return mongoose.connect(`${process.env.CONNECTION_STRING}`).then(() => console.log('connected'))
+    return mongoose.connect(`${process.env.CONNECTION_STRING}`).then(() => console.log('connected')).catch(error => console.log(error))
 }
 
 module.exports = runDB
